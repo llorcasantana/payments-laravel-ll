827dbef54274a021bd36a9258b537f65f42b93c6
Jose M. Llorca <jose.manuel@thecloud.group>
2023-01-30 16:12:28 -0500

    adjust redsys params

--- src/PaymentGateway.php
+++ src/PaymentGateway.php
@@ -6,19 +6,23 @@ use Llorcasantana\PaymentsLaravelLl\payments_libraries\Redsys;
 
 class PaymentGateway
 {
-    public string $url_dev = 'https://sis-t.redsys.es:25443/sis/rest/trataPeticionREST';
-    public string $url_prod = 'https://sis.redsys.es/sis/rest/trataPeticionREST';
     public string $payment_method = '';
     public string $payment_order = '';
     public string $payment_merchant_code = '';
-    public string $payment_currency = '';
-    public string $payment_ammount = '';
-    public string $payment_transaction_type = '';
-    public string $payment_terminal = '';
+    public string $payment_currency = '978';
+    public string $payment_redsys_method = 'T';
+    public string $payment_redsys_notif = 'http://localhost/';
+    public float $payment_amount = 0.00;
+    public string $payment_transaction_type = '0';
+    public string $payment_terminal = '1';
     public string $payment_url_ok = '';
     public string $payment_url_no_ok = '';
     public string $payment_api_url = '';
     public string $payment_api_url_ok = '';
+    public string $payment_version = 'HMAC_SHA256_V1';
+    public string $payment_trade_name = 'ECOMMERCE';
+    public string $payment_titular = 'Pedro Risco';
+    public string $payment_product_description = 'Compras varias';
     public string $payment_api_url_ko = '';
 
     public function loadData(): string
@@ -36,24 +40,30 @@ class PaymentGateway
      /*
      * PAYMENTS METHODS
      */
-    private function initRedsys(): array
+    private function initRedsys(): string
     {
-        $miObj = new Redsys();
-        $miObj->setParameter("DS_MERCHANT_AMOUNT", $this->payment_ammount);
-        $miObj->setParameter("DS_MERCHANT_ORDER", $this->payment_order);
-        $miObj->setParameter("DS_MERCHANT_MERCHANTCODE", $this->payment_merchant_code);
-        $miObj->setParameter("DS_MERCHANT_CURRENCY", $this->payment_currency);
-        $miObj->setParameter("DS_MERCHANT_TRANSACTIONTYPE", $this->payment_transaction_type);
-        $miObj->setParameter("DS_MERCHANT_TERMINAL", $this->payment_terminal);
-        $miObj->setParameter("DS_MERCHANT_MERCHANTURL", $this->payment_api_url);
-        $miObj->setParameter("DS_MERCHANT_URLOK", $this->payment_url_ok);
-        $miObj->setParameter("DS_MERCHANT_URLKO", $this->payment_url_no_ok);
-        $miObj->createMerchantParameters();
-        $claveSHA256 = 'sq7HjrUOBfKmC576ILgskD5srU870gJ7';
-        return [
-            'signature'=>$miObj->createMerchantSignature($claveSHA256),
-            'params'=>$miObj->createMerchantParameters(),
-            'result'=>true
-        ];
+        $key = 'sq7HjrUOBfKmC576ILgskD5srU870gJ7';
+        $redsys = new Redsys();
+
+        $redsys->setAmount($this->payment_amount);
+        $redsys->setOrder($this->payment_order);
+        $redsys->setMerchantcode($this->payment_merchant_code); //Reemplazar por el código que proporciona el banco
+        $redsys->setCurrency($this->payment_currency);
+        $redsys->setTransactiontype($this->payment_transaction_type);
+        $redsys->setTerminal($this->payment_terminal);
+        $redsys->setMethod($this->payment_redsys_method); //Solo pago con tarjeta, no mostramos iupay
+        $redsys->setNotification($this->payment_redsys_notif); //Url de notificacion
+        $redsys->setUrlOk($this->payment_api_url_ok); //Url OK
+        $redsys->setUrlKo($this->payment_api_url_ko); //Url KO
+        $redsys->setVersion($this->payment_version);
+        $redsys->setTradeName($this->payment_trade_name);
+        $redsys->setTitular($this->payment_titular);
+        $redsys->setProductDescription($this->payment_product_description);
+        $redsys->setEnviroment('live'); //Entorno test
+
+        $signature = $redsys->generateMerchantSignature($key);
+        $redsys->setMerchantSignature($signature);
+
+        return $redsys->createForm();
     }
 }
